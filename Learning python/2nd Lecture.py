58e8a5d654d00abbeb80675409c089a2af995394
Farhan1719 <farhankhankgf@gmail.com>
2026-08-27 05:17:30 +0530

    Till indexing

---
+++ Learning python/2nd Lecture.py
@@ -0,0 +1,26 @@
+#STRING
+str1= "This is a strings.\nwe are creating it in python."   #\n is a sequence character. which is used for next line. 
+print(str1)
+
+str1= "This is a strings.\twe are creating it in python."   #\t is same. for using Tab.
+print(str1)
+
+#BASIC OPERATION
+#Concatenation
+str1= "Farhan"
+str2= "Ahmad"
+final_str=str1+str2
+print(final_str)
+
+#Length of str
+str1="Farhan"                                               # using len(str) to find length of string. it includes character,digits,spaces,special character like $ % # @ etc.
+print(len(str1))
+
+str2="Ahmad"
+len1=len(str2)
+print(len1)
+
+#INDEXING
+
+
+
