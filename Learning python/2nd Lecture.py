f9729eaa2abd00f2a6e696ef0519406084ec1b28
Farhan1719 <farhankhankgf@gmail.com>
2026-08-28 16:52:45 +0530

    till negativ slicing

--- Learning python/2nd Lecture.py
+++ Learning python/2nd Lecture.py
@@ -21,6 +21,30 @@ len1=len(str2)
 print(len1)
 
 #INDEXING
+str= "Farhan Ahmad"                                         #Farhan FA  
+ch=str[0]                                                   #012345678
+print(ch)                                                   #indexing is a assining a no of your character including spaces or special character
+                                                            # str[x] , using this we can write specific character of assigned no, and here x is a assigned no
+ch=str[1]                                                   # And assigned no of a string is always starts with Zero.
+print(ch)
 
+print(str[6])
+
+#SLICING
+str= "Farhan Ahmad"                                         # str[a:b] , Using this write particilar character of a word, from character a to character b. are called slicing.
+print(str[1:6])                                             # where a and b are index assigning no.
+print(str[0:7])                                             # when we write a part of word from a word, str[a:b], here resultant word is always including a letter and excluding b letter.
+print(str[7:11])
+print(str[:7]) #[0:7]
+
+print(str[7:12])
+print(str[7:len(str)])
+print(str[7:]) #[7:len(str)]
+
+# negativ index slicing
+str="apple"                                                 #  a  p  p  l  e
+print(str[-3:-1])                                           # -5 -4 -3 -2 -1
+print(str[-5:-2])                                           str[a:b], here last letter got (-1) and first letter got (-5), and same as slicing it include firsh letter of firt digit (a) and exclude last letter of last digit (b)
+print(str[-3:-1])
 
 
